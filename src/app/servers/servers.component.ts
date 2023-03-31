ba01c50a12e24f489484fb2c12e1c7244b713737
Suresh <suresh08553@gmail.com>
2023-03-31 12:56:53 +0530

    typo in class name

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,6 +5,6 @@ import { Component } from "@angular/core";
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServerComponent {
+export class ServersComponent {
 
 }
