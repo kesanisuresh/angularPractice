5407094e1c2640b83cc96c636bc7957a710c40cb
Suresh <suresh08553@gmail.com>
2023-03-31 12:55:51 +0530

    manual component creation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServerComponent {
 
 }
